6ebc1f742dfc8d0b5a37fa927bec027e5b52c06a
russellgoldenberg <russellgoldenberg@gmail.com>
2019-09-08 18:29:28 -0400

    steps through choices and updates display

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,87 @@
+import * as d3 from "d3";
 import removeMobileHover from "./utils/removeMobileHover.js";
 import navImg from "./navImg.js";
 
-removeMobileHover();
-navImg();
+const $choices = d3.selectAll("[data-js='choice']");
+const $choiceButtons = $choices.selectAll("[data-js='choice-button']");
+const $backButton = d3.select("[data-js='back']");
+const $progressChoice = d3.select("[data-js='progress--choice']");
+const $progressTotal = d3.select("[data-js='progress--total']");
+const $progressResult = d3.select("[data-js='progress--result']");
+const $choiceSpan = $progressChoice.select("span");
+const $totalSpan = $progressTotal.select("span");
+const $resultSpan = $progressResult.select("span");
 
-document.querySelector("html").classList.add("js-is-loaded");
+const STATE = {
+  step: 0,
+  choice: [],
+};
+
+const STEP_COUNT = $choices.size();
+
+const TREE_MULTIPLIER = 10;
+
+const STORIES = ["wedding", "camping", "manufacturing"];
+
+let story = STORIES[0]; // default to wedding
+
+function toggleStep() {
+  const { step, choice } = STATE;
+
+  // show and hide different pieces of progress
+  $choices.attr("data-display", (d, i) => i === step ? "grid" : "none");
+  $progressChoice.attr("data-visibility", () => step > 0 ? "visible" : "hidden");
+  $progressTotal.attr("data-visibility", () => step >= 0 ? "visible" : "hidden");
+  $progressResult.attr("data-visibility", () => step === STEP_COUNT - 1 ? "visible" : "hidden");
+
+  // update text of each piece of progress
+  const total = d3.sum(choice);
+  const result = total * TREE_MULTIPLIER;
+  $choiceSpan.text(`${choice[choice.length - 1]} kg`);
+  $totalSpan.text(`${total} kg`);
+  $resultSpan.text(`${result} trees`);
+}
+
+// events
+function handleBack() {
+  STATE.choice.pop();
+  STATE.step -= 1;
+  STATE.step = Math.max(0 , STATE.step);
+  // console.log({STATE});
+  toggleStep();
+}
+
+function handleChoice() {
+  const $btn = d3.select(this);
+  const impact = +$btn.attr("data-impact");
+  STATE.choice.push(impact);
+  STATE.step += 1;
+  toggleStep();
+}
+
+function setupStory() {
+  // get story from url
+  const p = window.location.pathname;
+  const match = STORIES.find(s => p.includes(s));
+  story = match || story;
+
+  // show first step
+  toggleStep();
+}
+
+function setupEvents() {
+  $backButton.on("click", handleBack);
+  $choiceButtons.on("click", handleChoice);
+}
+
+function init() {
+  d3.select("html").classed("js-is-loaded", true);
+  removeMobileHover();
+  navImg();
+  setupStory();
+  setupEvents();
+}
+
+init();
+
+
